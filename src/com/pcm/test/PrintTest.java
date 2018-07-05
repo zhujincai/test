8e0993815ba056e0303b5e2fc3e76d4d7fc84f98
zhujincai <zhujincai@163.com>
2018-07-05 14:07:19 +0800

    初次提交

---
+++ src/com/pcm/test/PrintTest.java
@@ -0,0 +1,17 @@
+package com.pcm.test;
+
+public class PrintTest {
+
+	/**
+	 * @param args
+	 * 测试打印方法
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 输出helloworld
+		// 输入 sysout 然后按 Alt+/ 就可以快速的编写了
+		System.out.println("hello world");
+
+	}
+
+}
